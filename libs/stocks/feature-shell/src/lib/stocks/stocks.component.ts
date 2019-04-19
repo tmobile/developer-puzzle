6fa2a774c29415a3613f17105edd7f561ab7ee0c
Nitin Vericherla <nitin.vericherla@gmail.com>
2019-04-19 17:03:52 -0400

    feat(stock): Added typeahead and removed Go button

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -32,7 +32,9 @@ export class StocksComponent implements OnInit {
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.stockPickerForm.valueChanges.subscribe(this.fetchQuote);
+  }
 
   fetchQuote() {
     if (this.stockPickerForm.valid) {
